b263ccdafa4da4880901ef62bb76242829b91f8e
Abdullah Bendary <bendarryy@gmail.com>
2025-04-27 20:53:49 +0300

    ..

--- backend/Automated_Sys_tmp/automation_system/core/urls.py
+++ backend/Automated_Sys_tmp/automation_system/core/urls.py
@@ -8,12 +8,15 @@ urlpatterns = [
     path("register/", views.register_user, name="register"),  # Registration endpoint
     path("login/", views.login_user, name="login"),  # Login endpoint
     path("logout/", views.logout_user, name="logout"),  # Logout endpoint
-    path("create-system/", views.create_system, name="create_system"),  # System creation endpoint
-    path("systems/", views.SystemRetrieveView.as_view(), name="systems"),
+   
     # Employee
     path('<int:system_id>/invite/', views.EmployeeInviteView.as_view(), name='employee-invite'),
     path('<int:system_id>/employees/', views.EmployeeListView.as_view(), name='employee-list'),
     path('<int:system_id>/employees/<int:employee_id>/', views.EmployeeDeleteView.as_view(), name='employee-delete'),
     path('employee/login/', views.EmployeeLoginView.as_view(), name='employee-login'),
     path('employee/logout/', views.EmployeeLogoutView.as_view(), name='employee-logout'),
+    
+    
+    path("create-system/", views.create_system, name="create_system"),  # System creation endpoint
+    path("systems/", views.SystemRetrieveView.as_view(), name="systems"),
 ]
