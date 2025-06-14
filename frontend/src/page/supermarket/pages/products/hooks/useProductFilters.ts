201599d5b49da577a4ce22b8dba62fe230626b0c
Abdullah Bendary <92316698+bendarryy@users.noreply.github.com>
2025-06-14 18:04:50 +0300

    update api

--- frontend/src/page/supermarket/pages/products/hooks/useProductFilters.ts
+++
@@ -1,7 +0,0 @@
-import { useState } from 'react';
-import { ProductFilter } from './useProducts';
-
-export function useProductFilters() {
-  const [filter, setFilter] = useState<ProductFilter>('all');
-  return { filter, setFilter };
-}
