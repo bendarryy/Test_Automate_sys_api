7c3d5e5eed766abf407b9977bbce425d1ffa24b6
Abdullah Bendary <bendarryy@gmail.com>
2025-06-17 17:55:09 +0300

    Comment out CORS_ALLOWED_ORIGINS for improved configuration flexibility

--- automation_system/settings.py
+++ automation_system/settings.py
@@ -36,7 +36,7 @@ DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
-CORS_ALLOWED_ORIGINS = os.environ.get("CORS_ALLOWED_ORIGINS", "http://127.0.0.1:5173,http://localhost:5173").split(",")
+# CORS_ALLOWED_ORIGINS = os.environ.get("CORS_ALLOWED_ORIGINS", "http://127.0.0.1:5173,http://localhost:5173").split(",")
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_HEADERS = list(default_headers) + [
     'x-subdomain',
